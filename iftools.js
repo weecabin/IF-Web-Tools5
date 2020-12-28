67743d94628c497c6c353a5282cb2167c39b4aff
play.js <git@gituser.com>
2020-12-28 10:21:12 -0800

    024

--- iftools.js
+++ iftools.js
@@ -9,7 +9,7 @@ function execute()
 {
   document.getElementById("status").value="Working..."+(new Node("root")).ToXML();
   let txt= document.getElementById("txt");
-  txt.value="some shit for the textarea";
+  txt.value="<some shit for the textarea>";
   let fltplan = new FlightPlan("name");
   window.alert("in Execute");
   document.getElementById("txt").value=fltplan.ToXML();
