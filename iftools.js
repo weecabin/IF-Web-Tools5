38e6c6633565ead59242477bf46b9db08275bee1
play.js <git@gituser.com>
2020-12-28 11:00:19 -0800

    027

--- iftools.js
+++ iftools.js
@@ -7,7 +7,19 @@ function setup()
 
 function execute()
 {
-  document.getElementById("status").value="Working..."+(new Node("root")).ToXML();
+  let lat = document.getElementById("lat");
+  let lon = document.getElementById("lon");
+  let legs = document.getElementById("legs");
+  let radius = document.getElementById("radius");
+  let loops = document.getElementById("loops");
+  let status = document.getElementById("status"); 
+  
+  let legangle = Math.PI*2/Number(legs);
+  let leglen = 2*Math.sin(legangle/2)*radius;
+  
+  //HoldPattern(legs,leglen,lat,lon,loops=10)
+  status.value="Working: "+"HoldPattern("+concat([legs,leglen,lat,lon,loops],",")+")";
+  
   let txt= document.getElementById("txt");
   try 
   {
@@ -19,4 +31,17 @@ function execute()
     document.getElementById("txt").innerHTML = err.message;
   }
  
+}
+
+function concat(arrayofstrings,separator="")
+{
+  let ret = "";
+  let len = arrayofstrings.length;
+  for(i=0;i<len;i++)
+  {
+    ret += arrayofstrings[i];
+    if (i<(len-1))
+      ret+=separator;
+  }
+  return ret;
 }
