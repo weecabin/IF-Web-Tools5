4e5b094052ac718a3cee1c6ae616904ad989b42b
play.js <git@gituser.com>
2020-12-28 09:10:03 -0800

    using Flightplan

--- iftools.js
+++ iftools.js
@@ -8,6 +8,6 @@ function setup()
 function execute()
 {
   document.getElementById("status").value="Working..."+(new Node("root")).ToXML();
-  //let fltplan = new Flightplan("name");
-  document.getElementById("txt").value="<something></some>";
+  let fltplan = new Flightplan("name");
+  document.getElementById("txt").value=fltpln.ToXML();
 }
