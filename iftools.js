26dc6805934501574feace6aba77fce1b5c7d7cf
play.js <git@gituser.com>
2020-12-29 16:42:06 -0800

    013

--- iftools.js
+++ iftools.js
@@ -17,13 +17,13 @@ function test()
       if (this.readyState == 4 && this.status == 200) 
       {
         let jsontext = this.responseText;
-        document.getElementById("txt").innerHTML = jsontext;
+        document.getElementById("txt").innerHTML += jsontext;
         myAirports=JSON.parse(jsontext);
-        document.getElementById("testid").value=myAirports[0];
+        document.getElementById("testid").value=myAirports[0].icao+"="+myAirports[0].latitude+","myAirports[0].longitude;
       }
       else
       {
-        document.getElementById("txt").innerHTML="something wrong: "+this.readyState;
+        document.getElementById("txt").innerHTML+="something wrong: "+this.readyState;
       }
     };
     xhttp.open("GET", "MyAirports.json", true);
